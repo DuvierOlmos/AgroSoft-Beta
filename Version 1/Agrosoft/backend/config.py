6de74bfc62cf8669537c4d7a717089865503169e
DuvierOlmos <olmos.4561@gmail.com>
2025-09-09 17:16:34 -0500

    archivo base para API

---
+++ Version 1/Agrosoft/backend/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()  # carga .env en os.environ
+
+print(">>>> DB_HOST en entorno:", os.getenv("DB_HOST"))
+class Config:
+    SECRET_KEY  = os.getenv("SECRET_KEY",  "devkey")
+    DB_HOST     = os.getenv("DB_HOST",     "localhost")
+    DB_USER     = os.getenv("DB_USER",     "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_NAME     = os.getenv("DB_NAME",     "Agrosoft_BD")
+    DB_PORT     = os.getenv("DB_PORT",     "3306")
