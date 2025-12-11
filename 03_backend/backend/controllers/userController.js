83d360539890fbf87425329a9adb79b18735d2ae
DuvierOlmos <olmos.4561@gmail.com>
2025-12-11 16:52:09 -0500

    actualizando la estructura del repositorio

--- 03_backend/backend/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-const registerController = require("./registerController");
-const loginController = require("./loginController");
-
-module.exports = {
-  register: registerController.register,
-  login: loginController.login,
-};
